5de19030ca3b010f92edf5b4b47869cbdebb239d
JanyceMaria <143899242+04jjanyce@users.noreply.github.com>
2024-04-15 20:06:51 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+	public int soma (int a, int b) {
+		return a + b;
+	}
+	public int subtracao(int a, int b) {
+		return a - b;
+	}
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	public float dividir(int a, int b) {
+		return a/b;
+	}
+}
