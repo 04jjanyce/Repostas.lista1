b6c321c506928e042d65ee1098e2de802076e542
JanyceMaria <143899242+04jjanyce@users.noreply.github.com>
2024-04-15 20:54:07 -0300

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,21 @@
+public class Circulo {
+	private double raio;
+	
+	public Circulo(double r) {
+		this.raio=r;
+	}
+	
+	public double getRaio() {
+		return raio;
+	}
+	public void setRaio(double r) {
+		this.raio=r;
+	}
+	
+	public double calcularArea() {
+		return 3.14* (this.raio*this.raio);
+	}
+	public double calcularPerimetro(double r) {
+		return 2 * (3.14 * this.raio);
+	}
+}
